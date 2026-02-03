5658664f4f24cdec9d0c65e022e380d8cfc6f8d1
UJJWAL RAJ <ujjwalraj@UJJWALs-MacBook-Air.local>
2026-02-03 09:31:31 +0530

    Update

--- SelfExploration/DSA_Self/Arrays/021pairSumUnoptimised.cpp
+++ SelfExploration/DSA_Self/Arrays/021pairSumUnoptimised.cpp
@@ -28,7 +28,7 @@ int main(){
     vector<int> nums = {2, 7, 11, 15};
     int target = 13;
     vector<int> ans = pairSum(nums, target);
-    cout << ans[0] << ", " << ans[1];
+    cout << ans[0] << ", " << ans[1] << endl;
 
     return 0;
 }
