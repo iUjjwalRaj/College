9764575d45fb3dcf27bd5d02b41f6db957f979cc
UJJWAL RAJ <ujjwalraj@UJJWALs-MacBook-Air.local>
2026-02-05 11:06:15 +0530

    update

---
+++ Semester 2/Frontend & Backend Engineering/002_4th February, 2026/script.js
@@ -0,0 +1,50 @@
+// console.log(a);
+
+// var a = 90; // hoisting example
+
+
+
+// display(); 
+
+
+// function display(){
+//     console.log("HI");
+//     console.log(b);
+//     var b = 40;
+    
+// }
+// console.log(" "); 
+
+
+// console.log(c);
+// if(true){
+//     var c = 50;
+//     console.log(c);
+// }
+// console.log(c);
+
+
+// console.log(b);
+// display();
+console.log(c);
+console.log(b);
+display();
+function display(){
+    console.log("HI");
+    console.log(b);
+    var b = 90;
+    b++;
+    console.log(b);
+    c = 15;
+    console.log(c);
+
+    
+}
+
+
+if(true){
+    var c = 78;
+    // console.log(c);
+}
+console.log(c);
+
---
+++ Semester 2/Frontend & Backend Engineering/002_4th February, 2026/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+display();
+
+// function display(){
+//     console.log("HI");
+//     console.log(b);
+//     var b = 40;
+//     console.log(b);
+    
+// }
+
